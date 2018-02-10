3fecd7a1a1ebdebedf77897b9719d0f74cd920b8
Dominic <gonza362@gmail.com>
2018-02-10 14:51:16 -0700

    functional readme with images, and fixed a couple small things

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # bamazon
 MySQL/Node store 
+
+# bamazon Store Node App
+
+##OVERVIEW
+*bamazon* is a node application that allows the user to be a customer, manager, or supervisor to manipulate product and department data in a MySQL database. The bamazon database is contains two tables, *products* and *departments*, with respective column names of item_id, product_name, product_sales, department_name, price, stock_quantity, AND department_id, department_name, and over_head_costs.
+ 
+##REQUIREMENTS
+* latest version on node
+* npm install
+* MySQL Database *bamazon* with tables *products* and *departments*
+	1. The *products* table should have each of the following columns:
+		* item_id (unique id for each product)
+		* product_name (Name of product)
+		* department_name
+		* price (cost to customer)
+		* stock_quantity(how much of the product is available in stores)
+		* product_sales (how much $ made from specific item)
+	1. The *departments* table should include the following columns:
+		* department_id
+		* department_name
+		* over_head_costs  (A dummy number you set for each department)
+
+##FUNCTIONALITY
+##Customer View
+The customer application allows a user to “purchase” items available in the bamazon store.  The user is shown a table of all products (name), ID, and price, and then prompted to choose the ID of the product desired, and quantity to purchase. The user is notified of an incorrect ID or insufficient stock to fulfill order. Then the total cost is calculated and the user is then asked to confirm purchase. After the purchase is confirmed, or not, they are asked if they would like to buy anything else. If yes, then they will go through he same process, otherwise they will be thanked and exited from the application.
+
+![Customer Image](/readmeImages/customerImage.png)
+
+##Manager View
+The manager application provides options for the user to:
+    * View Products for Sale
+    * View Low Inventory
+    * Add to Inventory
+    * Add New Product
+
+If a manager selects `View Products for Sale`, the app lists every available item: the item IDs, names, prices, and quantities.
+
+![Manager View Inventory](/readmeImages/managerViewProducts.png)
+
+
+If a manager selects `View Low Inventory`, then it lists all items with an inventory count lower than five.
+
+![Manager Low Inventory](/readmeImages/managerLowInventory.png)
+
+If a manager selects `Add to Inventory`, the app displays a prompt that will let the manager "add more" of any item currently in the store.
+
+![Manager Add Inventory](/readmeImages/managerAddInventory.png)
+
+If a manager selects `Add New Product`, it allows the manager to add a completely new product to the store.
+
+![Manager Add Product](/readmeImages/managerAddProduct.png)
+
+##Supervisor View
+The supervisor application will first will list a set of menu options:
+   * View Product Sales by Department
+   * Create New Department
+
+If a supervisor selects `View Product Sales by Department`, the app displays a summarized table grouped by department in their terminal/bash window showing the department ID, department name, over head costs, product sales, and total profit
+
+![Supervisor View Sales](/readmeImages/supervisorSummary.png)
+
+If a supervisor selects ‘Create New Department’, it allows the manager to add a completely new department to the store.
+
+![Supervisor View Add Department](/readmeImages/supervisorAddDepartment.png)
+
+##BUILT WITH
+*node
+*MySQL
+*JavaScript
+
+##CONTRIBUTING
+*GUI interface
+*user authentication
+* refactor code to have a cart with all products at checkout instead of completing order for each product
+*Any other awesome ideas
+
+##Contributors
+Dominic Gonzalez-Padron
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,10 @@
-const inquirer = require('inquirer');
-var Table = require('cli-table');
-const mysql = require('mysql2');
+const inquirer = require("inquirer");
+var Table = require("cli-table");
+const mysql = require("mysql2");
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'bamazon'
+  host: "localhost",
+  user: "root",
+  database: "bamazon"
 });
 let chosenItem;
 let customerProduct;
@@ -13,152 +13,156 @@ let totalCost;
 let newQuantity;
 
 function showProducts() {
-    // show the products available in the database
-    connection.query(
-        'SELECT * FROM `products`', function (err, results) {
-            displayTable(results);
-            // ask what product the customer would like to purchase
-            buySomething(results);
-        });
+  // show the products available in the database
+  connection.query("SELECT * FROM `products`", function(err, results) {
+    displayTable(results);
+    // ask what product the customer would like to purchase
+    buySomething(results);
+  });
 }
 function displayTable(results) {
-    var table = new Table({
-        head: ['Item ID', 'Product Name', 'Price', 'Stock']
-        , colWidths: [10, 30, 10, 10]
-    });
-    for (i = 0; i < results.length; i++) {
-        table.push(
-            [results[i].item_id, results[i].product_name, results[i].price]
-        );
-    }
-    console.log(table.toString());
+  var table = new Table({
+    head: ["Item ID", "Product Name", "Price"],
+    colWidths: [10, 30, 10]
+  });
+  for (i = 0; i < results.length; i++) {
+    table.push([results[i].item_id, results[i].product_name, results[i].price]);
+  }
+  console.log(table.toString());
 }
 
 function buySomething(results) {
-    inquirer.prompt([
-        {
-            name: "product_id",
-            type: "input",
-            message: "Please provide the ID of the item you would like to purchase."
-        }, {
-            name: "product_quantity",
-            type: "input",
-            message: "How many would you like to purchase?"
+  inquirer
+    .prompt([
+      {
+        name: "product_id",
+        type: "input",
+        message: "Please provide the ID of the item you would like to purchase."
+      },
+      {
+        name: "product_quantity",
+        type: "input",
+        message: "How many would you like to purchase?"
+      }
+    ])
+    .then(answers => {
+      // check to see if ID selection is valid
+      // console.log('answers: ', answers);
+      // get the chosen product
+      customerProduct = +answers.product_id;
+      customerQuantity = +answers.product_quantity;
+      // console.log(results);
+      // check to see  if ID selection is valid
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].item_id === customerProduct) {
+          chosenItem = results[i];
         }
-    ]).then(answers => {
-        // check to see if ID selection is valid
-        // console.log('answers: ', answers);
-        // get the chosen product
-        customerProduct = +answers.product_id;
-        customerQuantity = +answers.product_quantity;
-        // console.log(results);
-        // check to see  if ID selection is valid
-        for (var i = 0; i < results.length; i++) {
-            if (results[i].item_id === customerProduct) {
-                chosenItem = results[i];
-            }
-        }
-        if (chosenItem == undefined) {
-            console.log('=====================================');
-            console.log("Please choose a valid ID.");
-            console.log('=====================================');
-            showProducts();
+      }
+      if (chosenItem == undefined) {
+        console.log("=====================================");
+        console.log("Please choose a valid ID.");
+        console.log("=====================================");
+        showProducts();
+      } else {
+        // check if there is enough stock for the order
+        if (chosenItem.stock_quantity >= customerQuantity) {
+          // console.log("We have enough in stock for this order.")
+          // determine total cost
+          totalCost = chosenItem.price * customerQuantity;
+          // console.log(totalCost);
+          newQuantity = chosenItem.stock_quantity - customerQuantity;
+          // console.log('newQuantity: ', newQuantity);
+          // ask if total coast is okay and to confirm order
+          confirmPurchase();
         } else {
-            // check if there is enough stock for the order
-            if (chosenItem.stock_quantity >= customerQuantity) {
-                // console.log("We have enough in stock for this order.")
-                // determine total cost
-                totalCost = chosenItem.price * customerQuantity;
-                // console.log(totalCost);
-                newQuantity = chosenItem.stock_quantity - customerQuantity;
-                // console.log('newQuantity: ', newQuantity);
-                // ask if total coast is okay and to confirm order
-                confirmPurchase();
-            } else {
-                console.log('=====================================');
-                console.log("Insufficient quantity in stock!");
-                console.log('=====================================');
-                // START OVER???
-                nextOrder();
-            }
+          console.log("=====================================");
+          console.log("Insufficient quantity in stock!");
+          console.log("=====================================");
+          // START OVER???
+          nextOrder();
         }
-        // console.log(chosenItem);
+      }
+      // console.log(chosenItem);
     });
 }
 function confirmPurchase() {
-    inquirer.prompt([
-        {
-            name: "confirmOrder",
-            type: "confirm",
-            message: `Your total cost is $${totalCost}. Do you wish to complete this purchase?`
-        }
-    ]).then(answer => {
-        console.log('answer: ', answer);
-        if (answer.confirmOrder) {
-            console.log('=====================================');
-            console.log("Your order has been confirmed")
-            console.log('=====================================');
-            // update stock in database
-            updateStock();
-            updateSales();
-            nextOrder();
-        }
-        else {
-            console.log('=====================================');
-            console.log("Your order has been canceled")
-            console.log('=====================================');
+  inquirer
+    .prompt([
+      {
+        name: "confirmOrder",
+        type: "confirm",
+        message: `Your total cost is $${totalCost}. Do you wish to complete this purchase?`
+      }
+    ])
+    .then(answer => {
+      //   console.log("answer: ", answer);
+      if (answer.confirmOrder) {
+        console.log("=====================================");
+        console.log("Your order has been confirmed");
+        console.log("=====================================");
+        // update stock in database
+        updateStock();
+        updateSales();
+        nextOrder();
+      } else {
+        console.log("=====================================");
+        console.log("Your order has been canceled");
+        console.log("=====================================");
 
-            // start over?????
-            nextOrder();
-        }
-    })
+        // start over?????
+        nextOrder();
+      }
+    });
 }
 function updateStock() {
-    connection.query(
-        'UPDATE `products` SET ? WHERE ?',
-        [
-            { stock_quantity: newQuantity },
-            { item_id: chosenItem.item_id }
-        ],
-        (err) => {
-            if (err) { console.log(err); }
-        }
-    )
+  connection.query(
+    "UPDATE `products` SET ? WHERE ?",
+    [{ stock_quantity: newQuantity }, { item_id: chosenItem.item_id }],
+    err => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 }
 function updateSales() {
-    connection.query(
-        'UPDATE products SET product_sales = product_sales + ' + totalCost + ' WHERE item_id = ' + chosenItem.item_id,
-        (err) => {
-            if (err) { console.log(err); }
-        }
-    )
+  connection.query(
+    "UPDATE products SET product_sales = product_sales + " +
+      totalCost +
+      " WHERE item_id = " +
+      chosenItem.item_id,
+    err => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 }
-
 
 function nextOrder() {
-    inquirer.prompt([
-        {
-            name: "anotherOrder",
-            type: "confirm",
-            message: `Would you like to buy anything else?`
-        }
-    ]).then(answer => {
-        // console.log('answer: ', answer);
-        if (answer.anotherOrder) {
-            showProducts();
-        }
-        else {
-            console.log('=====================================');
-            console.log("Thank You, Come Again!")
-            console.log('=====================================');
-            process.exit();
-        }
-    })
+  inquirer
+    .prompt([
+      {
+        name: "anotherOrder",
+        type: "confirm",
+        message: `Would you like to buy anything else?`
+      }
+    ])
+    .then(answer => {
+      // console.log('answer: ', answer);
+      if (answer.anotherOrder) {
+        showProducts();
+      } else {
+        console.log("=====================================");
+        console.log("Thank You, Come Again!");
+        console.log("=====================================");
+        process.exit();
+      }
+    });
 }
 
-console.log('=====================================');
-console.log('-----  WELCOME TO CLIMB-MART!  ------')
-console.log('=====================================');
+console.log("=====================================");
+console.log("-----  WELCOME TO CLIMB-MART!  ------");
+console.log("=====================================");
 
 showProducts();
-
